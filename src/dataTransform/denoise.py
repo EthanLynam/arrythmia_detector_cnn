9ace4f699b5645f352663757ac28aff5c377aa8b
Ethan <etholymo@gmail.com>
2024-11-11 06:41:11 +0000

    functionality added

---
+++ src/dataTransform/denoise.py
@@ -0,0 +1,22 @@
+from pywt import wavedec
+import pywt
+import numpy 
+
+
+def denoise_signal(ECG_data):
+   
+    coefficients = wavedec(ECG_data, 'bior4.4', level=10)  # decompose signal into wavelt coefficients
+
+    low_cutoff = 1
+    high_cutoff = 9
+
+    # zero out coeffocoents from 0 to low cutoff 
+    for num in range(0, low_cutoff):
+        coefficients[num] = numpy.multiply(coefficients[num],[0.0])
+    
+    # zero out coefficients from high cutoff to end
+    for num in range(high_cutoff, len(coefficients)):
+        coefficients[num] = numpy.multiply(coefficients[num], 0)
+
+    denoised_ECG = pywt.waverec(coefficients, 'bior4.4')  # inverse wavelet transform to reconstruct the signal, now denoised
+    return denoised_ECG
