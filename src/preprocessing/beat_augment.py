a780f6a314addd05bf3cf85a4a395aeac79a2607
Ethan <etholymo@gmail.com>
2025-01-26 19:37:05 +0000

    modified beat_augment to include image creation for augmented beats that better suits CNN data segmentation

--- src/preprocessing/beat_augment.py
+++ src/preprocessing/beat_augment.py
@@ -19,6 +19,8 @@ def beat_augment(beat_data, beat_label, idx, patient_num):
     if beat_label == 'N':
         return
 
+    print(f"            Arrythmia detected - augmented images created from heartbeat {idx}")
+
     # take the original beat and stretch it by 1.25
     x_original = numpy.arange(len(beat_data))
     x_stretched = numpy.linspace(0, len(beat_data) - 1, int(len(beat_data) * 1.25))
@@ -52,33 +54,76 @@ def beat_augment(beat_data, beat_label, idx, patient_num):
     new_positive_beat_stretched = beat_stretched * poly_positive_stretched
     new_negative_beat_stretched = beat_stretched * poly_negative_stretched
 
-    # Create directories
-    direct = f'../../created-images/Edited/Patient_{patient_num}/Beat_{idx}'
-    os.makedirs(direct, exist_ok=True)
+# THIS SECTION LABELS THE AUGMENTED BEATS AND PLACES THEM IN DISTINCTIVE FOLDERS
+# INSIDE OF CREATED_IMAGES -> EDITED. IT IS FOR VIEWING PURPOSES AND TO SEE 
+# THE DIFFERENCES BETWEEN EACH IMAGE IN COMPARISON TO THE ORIGINAL AND EACH OTHER.
+
+#    # Create directories
+#    direct = f'../../created-images/Edited/Patient_{patient_num}/Beat_{idx}'
+#    os.makedirs(direct, exist_ok=True)
+#
+#    # Add the normal beat to the list of images to save
+#    pairs = [
+#        (beat_data, "normal"),  # Normal beat
+#        (new_positive_beat, "original_p"),
+#        (new_negative_beat, "original_n"),
+#        (new_positive_beat_stretched, "stretched_p"),
+#        (new_negative_beat_stretched, "stretched_n"),
+#    ]
+#
+#    # Save all images
+#    for data, name in pairs:
+#        fig, ax = plt.subplots(figsize=(1.66, 1.38), dpi=100)
+#        ax.plot(data, color='black')
+#        ax.set_title(name)
+#        ax.set_xlim(0, len(data))
+#        ax.axis('off')
+#
+#        fig.savefig(
+#            f'../../created-images/Edited/Patient_{patient_num}/Beat_{idx}/{name}.png',
+#            bbox_inches='tight',
+#            pad_inches=0
+#            )
+#
+#        plt.close(fig)
+
+# THIS SECTION LABELS ALL BEATS BY THEIR TYPES AND PLACES THEM INSIDE GENERIC 
+# CLASS FOLDERS (NOR, LBB, RBB ... VFW) INSIDE OF CREATED-IMAGES -> EDITED.
+# IT IS FOR THE CNN TO DIVIDE THEM INTO TRAINING, VAL, OR TESTING DATA.
+
+    annotation_map = {
+            'N': 'NOR', 
+            'V': 'PVC', 
+            '/': 'PAB', 
+            'R': 'RBB', 
+            'L': 'LBB',
+            'A': 'APC', 
+            '!': 'VFW', 
+            'E': 'VEB'
+        }
+    full_name = annotation_map.get(beat_label, 'OTHER')
 
     # Add the normal beat to the list of images to save
     pairs = [
         (beat_data, "normal"),  # Normal beat
-        (new_positive_beat, "original_p"),
-        (new_negative_beat, "original_n"),
-        (new_positive_beat_stretched, "stretched_p"),
-        (new_negative_beat_stretched, "stretched_n"),
+        (new_positive_beat, "Op"), # Op = Original positive polynomial
+        (new_negative_beat, "On"), # On = Original negatuve polynomial
+        (new_positive_beat_stretched, "Sp"), # Sp = Stretched & positive polynomial
+        (new_negative_beat_stretched, "Sn"), # Sn = Stretched & negative polynomial
     ]
 
     # Save all images
     for data, name in pairs:
         fig, ax = plt.subplots(figsize=(1.66, 1.38), dpi=100)
         ax.plot(data, color='black')
-        ax.set_title(name)
+        ax.set_title(full_name)
         ax.set_xlim(0, len(data))
         ax.axis('off')
 
         fig.savefig(
-            f'../../created-images/Edited/Patient_{patient_num}/Beat_{idx}/{name}.png',
+            f'../../created-images/Edited/{full_name}/{patient_num}_{idx}_{name}.png',
             bbox_inches='tight',
             pad_inches=0
             )
 
         plt.close(fig)
-
-    print("      Augmented beats created.")
