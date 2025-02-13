99e8edf6114d5a38c5ed291af3f80e7bf6513560
Ethan <etholymo@gmail.com>
2025-02-13 17:06:57 +0000

    updates

--- src/cnn/cnn_model.py
+++ src/cnn/cnn_model.py
@@ -3,18 +3,9 @@
 import tensorflow as tf
 
 def create_cnn():
-    """
-    Builds and returns a CNN model.
-
-    Args:
-        input_shape (tuple): Shape of input images (height, width, channels).
-        num_classes (int): Number of output classes.
-
-    Returns:
-        model (tf.keras.Model): Compiled CNN model.
-    """
 
     model = tf.keras.Sequential([
+
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(128, 128, 1)),
         tf.keras.layers.MaxPooling2D((2, 2)),
 
@@ -26,8 +17,7 @@ def create_cnn():
 
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
-        # tf.keras.layers.Dropout(0.5),  # Prevents overfitting
-        tf.keras.layers.Dense(8, activation='softmax')  # Output layer (num of classes, activation function)
+        tf.keras.layers.Dense(8, activation='softmax')
     ])
 
     model.compile(optimizer='adam',
